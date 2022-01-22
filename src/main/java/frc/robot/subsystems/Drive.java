dfc71f0baacd24a83c90f2729ec1cd03d9a002a0
team5417 <matthewcalda@gmail.com>
2022-01-22 17:38:23 -0600

    Fixed motors

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -6,39 +6,31 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import java.lang.Math;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase {
   /** Creates a new Drivetrain. */
-  private CANSparkMax driveSlaveL = new CANSparkMax(Constants.slaveLeftMotor, MotorType.kBrushless);
-  private CANSparkMax driveMasterR = new CANSparkMax(Constants.masterRightMotor, MotorType.kBrushless);
-  private CANSparkMax driveSlaveR = new CANSparkMax(Constants.slaveRightMotor, MotorType.kBrushless);
-  private CANSparkMax driveMasterL = new CANSparkMax(Constants.masterLeftMotor, MotorType.kBrushless);
-
-  private void setIdleModes(IdleMode mode) {
-    driveMasterL.setIdleMode(mode);
-    driveMasterR.setIdleMode(mode);
-    driveSlaveL.setIdleMode(mode);
-    driveSlaveR.setIdleMode(mode);
-  }
+  private TalonSRX driveSlaveL = new TalonSRX(Constants.slaveLeftMotor);
+  private TalonSRX driveSlaveR = new TalonSRX(Constants.slaveRightMotor);
+  private TalonSRX driveMasterL = new TalonSRX(Constants.masterLeftMotor);
+  private TalonSRX driveMasterR = new TalonSRX(Constants.masterRightMotor);
 
   public Drive() {
     driveSlaveL.follow(driveMasterL);
     driveSlaveR.follow(driveMasterR);
-    setIdleModes(IdleMode.kCoast);
   }
 
   public void rawMotorPower(double leftPower, double rightPower) {
-    driveMasterL.set(leftPower);
-    driveMasterR.set(rightPower);
+    
+    driveMasterL.set(ControlMode.PercentOutput, leftPower);
+    driveMasterR.set(ControlMode.PercentOutput, rightPower);
   }
 
   public void setPower(double leftPower, double rightPower){
-    rawMotorPower(-Math.pow(leftPower, 3), Math.pow(rightPower, 3));
+    rawMotorPower(-Math.pow(leftPower, 1), Math.pow(rightPower, 1));
   }
 }
