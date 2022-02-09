d6b5fb421d99a0c51f75a7136c051de41f4e7392
eyangch <eyangch@gmail.com>
2022-02-08 18:59:54 -0600

    Add unused stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.Orient;
 import frc.robot.commands.TankDrive;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Drive;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -23,11 +21,9 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final Drive drive;
   private final Limelight limelight;
   
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
   private final TankDrive tankDrive;
   private final Orient orient;
 
@@ -92,6 +88,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
